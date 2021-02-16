aad4b92b4fa27ee478d572d9c9a2a3669d9f0601
hanskerkhof <hans@netmatic.nl>
2021-02-16 13:03:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# priva-muscle-assignment-dzxhzp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/priva-muscle-assignment-dzxhzp)
---
+++ src/app/muscle-number/muscle-number.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-muscle-number",
+  templateUrl: "./muscle-number.component.html",
+  styleUrls: ["./muscle-number.component.css"]
+})
+export class MuscleNumberComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
